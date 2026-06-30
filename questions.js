367b9676d90111b201eaa68393fc426e0328b17d
JSLegalForce <info@jslegalforce.nl>
2026-06-30 20:40:23 +0200

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,68 @@
+const ALLE_VRAGEN = [
+  {
+    thema: "Toepassingsbereik",
+    vraag: "Een veiligheidsfouillering kan worden toegepast bij een slachtoffer, getuige of verdachte, maar uitsluitend wanneer de ambtenaar optreedt als opsporingsambtenaar op grond van het Wetboek van Strafvordering.",
+    opties: [
+      "Deze stelling klopt niet",
+      "Deze stelling klopt"
+    ],
+    juist: 0,
+    uitleg: "Een veiligheidsfouillering (art. 7 lid 3 Politiewet 2012) is niet uitsluitend gekoppeld aan de opsporingstaak op grond van het Wetboek van Strafvordering. Deze bevoegdheid kan ook worden toegepast in andere situaties, bijvoorbeeld bij een persoon die onbegrepen gedrag vertoont en waarbij de veiligheid van de persoon zelf, de ambtenaar of anderen in het geding kan zijn. Een veiligheidsfouillering kan dus worden toegepast bij verschillende categorieën personen en is niet beperkt tot verdachten.",
+    artikel: "Art. 7 lid 3 Politiewet 2012"
+  },
+  {
+    thema: "Wijze van onderzoek",
+    vraag: "Bij een veiligheidsfouillering mag je een persoon aan de kleding onderzoeken. Daarom mag je ook in de kleding kijken of deze doorzoeken.",
+    opties: [
+      "Deze stelling klopt",
+      "Deze stelling klopt niet"
+    ],
+    juist: 1,
+    uitleg: "Bij een veiligheidsfouillering mag de kleding uitsluitend oppervlakkig worden afgetast (artikel 21, eerste lid, Ambtsinstructie). Het is niet toegestaan om in de kleding te kijken of deze te doorzoeken. Voel je tijdens het aftasten een voorwerp dat een gevaar voor de veiligheid kan opleveren, dan mag je dit voorwerp uiteraard uit de kleding halen.",
+    artikel: "Art. 21 lid 1 Ambtsinstructie"
+  },
+  {
+    thema: "Aangetroffen voorwerpen",
+    vraag: "Als je tijdens een veiligheidsfouillering een scherp voorwerp aantreft, moet je dit in beslag nemen.",
+    opties: [
+      "Deze stelling klopt niet",
+      "Deze stelling klopt"
+    ],
+    juist: 0,
+    uitleg: "Wanneer tijdens een veiligheidsfouillering een gevaarlijk voorwerp wordt aangetroffen, hoeft dit niet altijd strafrechtelijk in beslag te worden genomen. Als het voorwerp geen verboden wapen is en er geen strafvorderlijke grond voor inbeslagneming bestaat, mag het voorwerp tijdelijk in bewaring worden genomen op grond van artikel 20, tweede lid, van de Ambtsinstructie. Het voorwerp wordt dan uitsluitend uit veiligheidsoverwegingen onder de ambtenaar gehouden gedurende de ambtsverrichting.",
+    artikel: "Art. 20 lid 2 Ambtsinstructie"
+  },
+  {
+    thema: "Doel van de fouillering",
+    vraag: "Het doel van een veiligheidsfouillering is het opsporen en in beslag nemen van verboden wapens.",
+    opties: [
+      "Deze stelling klopt",
+      "Deze stelling klopt niet"
+    ],
+    juist: 1,
+    uitleg: "Het doel van een veiligheidsfouillering is om te onderzoeken of een persoon gevaarlijke voorwerpen bij zich draagt die een gevaar kunnen opleveren voor de veiligheid van de betrokkene, de ambtenaar of anderen. De veiligheidsfouillering is dus primair gericht op het waarborgen van de veiligheid en niet specifiek op het opsporen en in beslag nemen van verboden wapens.",
+    artikel: "Doel veiligheidsfouillering"
+  },
+  {
+    thema: "Meldplicht",
+    vraag: "Een veiligheidsfouillering of een onderzoek aan zaken die een persoon met zich voert, moet onverwijld schriftelijk worden gemeld aan de hulpofficier van justitie, inclusief de redenen die tot dit onderzoek hebben geleid.",
+    opties: [
+      "Deze stelling klopt",
+      "Deze stelling klopt niet"
+    ],
+    juist: 0,
+    uitleg: "De ambtenaar die een veiligheidsfouillering heeft uitgevoerd of een onderzoek heeft verricht aan zaken die een persoon met zich voert, meldt dit onverwijld schriftelijk aan de hulpofficier van justitie. In deze melding worden de redenen opgenomen die aanleiding hebben gegeven tot het onderzoek (artikel 21 Ambtsinstructie).",
+    artikel: "Art. 21 Ambtsinstructie"
+  },
+  {
+    thema: "Inbeslagname en BOA Domein I",
+    vraag: "Als BOA Domein I mag ik een tijdens een veiligheidsfouillering aangetroffen verboden wapen, zoals een vlindermes of een boksbeugel, niet in beslag nemen.",
+    opties: [
+      "Deze stelling klopt niet",
+      "Deze stelling klopt"
+    ],
+    juist: 1,
+    uitleg: "Als boa Domein I ben je niet bevoegd om verboden wapens, zoals een vlindermes of een boksbeugel, op grond van de Wet wapens en munitie strafrechtelijk in beslag te nemen. Je bent immers niet belast met de opsporing van deze wetgeving. Wel kun je dergelijke voorwerpen tijdelijk in bewaring nemen op grond van artikel 20, tweede lid, van de Ambtsinstructie, omdat deze een gevaar voor de veiligheid kunnen opleveren. De voorwerpen kunnen vervolgens worden overgedragen aan de politie, die bevoegd is om deze strafrechtelijk in beslag te nemen.",
+    artikel: "Art. 20 lid 2 Ambtsinstructie / Wet wapens en munitie"
+  }
+];
